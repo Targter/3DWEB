a9016114bb56825d96c7c4237fdd8b279050fd23
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-04-11 01:35:39 +0530

    Update Description.jsx

--- src/component/Description.jsx
+++ src/component/Description.jsx
@@ -3,28 +3,28 @@ import Descriptioncontent from "./Descriptioncontent";
 const Description = ({ changebackground }) => {
   const container = [
     {
-      img1: "public/img1.png",
+      img1: "img1.png",
       Description1: "Credit Bureau Analysis:",
       extracontent:
         "Think of the Credit Bureau Analysis as the logbook of your previous expeditions. It sheds light on how you, as the captain, have navigated past financial commitments.",
       duration: 0.4,
     },
     {
-      img1: "public/img2.png",
+      img1: "img2.png",
       Description1: "Financial Analysis: ",
       extracontent:
         "Diving into the Financial Analysis of your company is akin to inspecting the hull of your ship. It involves a thorough examination of your financial statements, revenue streams, ",
       duration: 0.3,
     },
     {
-      img1: "public/img4.jpg",
+      img1: "img4.jpg",
       Description1: "Banking Analysis: ",
       extracontent:
         "The GST Analysis can be likened to understanding the trade winds that propel your ship forward. It involves scrutinizing your Goods and Services Tax (GST) ",
       duration: 0.2,
     },
     {
-      img1: "public/img3.png",
+      img1: "img3.png",
       Description1: "GST Analysis:",
       extracontent:
         "Lastly, the Banking Analysis is your navigational chart, offering a detailed view of your financial transactions,",
